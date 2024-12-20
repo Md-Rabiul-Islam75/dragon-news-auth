eba56570c55a619587735d8646acd800011f949a
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-12-20 11:11:51 +0600

    recap implement logout and login feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import userIcon from "../assets/user.png";
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 const Navbar = () => {
-  const { user } = useContext(AuthContext); 
+  const { user,logOut } = useContext(AuthContext); 
   console.log(user);
   return (
     <div className="flex justify-between items-center">
@@ -17,7 +17,13 @@ const Navbar = () => {
         <div className=" ">
           <img src={userIcon} alt="" />
         </div>
-        <Link to="/auth/login" className="btn btn-neutral rounded-none">Login</Link>
+        {
+          user && user?.email ?
+           <button onClick={logOut} className="btn btn-neutral rounded-none">Log-Out</button>
+            : 
+            <Link to="/auth/login" className="btn btn-neutral rounded-none">Login</Link>
+        }
+        
       </div>
     </div>
   );
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -14,10 +14,22 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const userLogin = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+
+
     const authInfo={
         user,
         setUser,
-        createNewUser
+        createNewUser,
+        logOut,
+        userLogin,
     };
 
     useEffect(() =>{
